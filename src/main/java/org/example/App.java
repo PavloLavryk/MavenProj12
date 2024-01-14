ef1476827488d59891ddd91e08c43347faf3d148
pavlo <pavlolavryk@gmail.com>
2024-01-14 21:21:15 +0200

    1

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class App {
+    public static void main(String[] args) {
+        User user = new User("Pavlo", "Lavryk");
+
+        Gson gson = new Gson();
+        System.out.println(gson.toJson(user));
+    }
+}
---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class User {
+    public String name;
+    public String lastName;
+
+    public User(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+}
